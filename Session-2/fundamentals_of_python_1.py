49e5c65663610d35e4706817107843f2ba276c95
Sam <Svangorden13@gmail.com>
2024-01-10 09:51:44 -0800

    Creating repository containing lecture slides and presentation materials for courses in data science

---
+++ Session-2/fundamentals_of_python_1.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+## <center>Python Control Flow </center>
+
+# Exercise-1: Write a program that asks the user what kind of rental car they would like. Print a message about that car, 
+# such as “Let me see if I can find you a Subaru.”
+
+# Exercise-2: Ask the user for a number, and then report whether the number is a multiple of 10 or not.
+
+# Exercise-3:  Create an grader that prints the letter grade for an input score. (Hint grade = input("Enter a score: ")
+# 0 - 59 = F 
+# 60 - 69 = D 
+# 70 - 79 = C 
+# 80 - 89 = B 
+# 90 - 100 = A
+
+# Exercise-4: Usernames: Do the following to create a program that simulates how websites ensure that everyone has a unique username.
+# Make a list of five or more usernames called current_users.
+# Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
+# Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a
+# new username. If a username has not been used, print a message saying that the username is available.
+# Make sure your comparison is case insensitive. If 'John' has been used,'JOHN' should not be accepted.
+
+# Exercise-5: A movie theater charges different ticket prices depending on a person’s age. If a person is under the age of 7, 
+# the ticket is free, if they are between 7 and 18, the ticket is \$10, and if they are over age 18, the ticket is $15. 
+# Write a loop in which you ask users their age, and then tell them the cost of their movie ticket.
+
